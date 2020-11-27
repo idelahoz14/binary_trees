009292c4c234cce9412809929156210b0090037e
idelahoz14 <idelahoz14@cuc.edu.co>
2020-11-27 02:44:09 +0000

    fixing code(Betty style)

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,13 +11,13 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *grand_father;
 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-    {
+	{
 		return (NULL);
-    }
+	}
 	grand_father = node->parent->parent;
 	if (grand_father->left == node->parent)
-    {
+	{
 		return (grand_father->right);
-    }
+	}
 	return (grand_father->left);
 }
