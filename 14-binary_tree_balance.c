f7603fd5e7ddc5c476af8f2fe24683d42e090c2d
idelahoz14 <idelahoz14@cuc.edu.co>
2020-11-27 02:21:40 +0000

    task number 14(Trying another solution)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,22 +6,38 @@
  *
  * Return: 0
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_height_1(const binary_tree_t *tree)
 {
-	int left = 0;
-	int right = 0;
+
+	int count = 0, count2 = 0;
 
 	if (tree == NULL)
 	{
+		return (-1);
+	}
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		count = binary_tree_height_1(tree->left) + 1;
+		count2 = binary_tree_height_1(tree->right) + 1;
+	}
+	if (count > count2)
+	{
+		return (count);
+	}
+
+	return (count2);
+}
+
+/**
+ * binary_tree_balance - function that counts the node with childs
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: the count of the nodes
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
 		return (0);
-	}
-	else if (tree->left)
-	{
-		left += 1 + binary_tree_balance(tree->left);
-	}
-	if (tree->right)
-	{
-		right += 1 + binary_tree_balance(tree->right);
-	}
-	return (left - right);
+	return (binary_tree_height1(tree->left) - binary_tree_height1(tree->right));
 }
