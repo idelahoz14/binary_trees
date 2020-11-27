9b1408b402250662f46d80c14b81938b8219f1a0
idelahoz14 <idelahoz14@cuc.edu.co>
2020-11-27 01:44:05 +0000

    task number 14(Balance factor)

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: 0
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left = 0;
+	int right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else if (tree->left)
+	{
+		left += 1 + binary_tree_balance(tree->left);
+	}
+	if(tree->right)
+	{
+		right += 1 + binary_tree_balance(tree->right);
+	}
+
+	return (left - right);
+}
